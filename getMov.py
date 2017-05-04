a863647f13ed3949f70a590c416f809bced850df
kannawin <scott.kannawin@ou.edu>
2017-05-04 18:27:16 -0500

    Returns Title

--- getMov.py
+++ getMov.py
@@ -17,25 +17,16 @@ pairs = {
 	"language" : "en-US",
 }
 
-values = []
-values.append('api_key')
-values.append('year')
-values.append('include_adult')
-values.append('query')
-values.append('language')
-
-
 queryString = urllib.urlencode(pairs)
 
-cheatURL = 'https://api.themoviedb.org/3/search/movie?api_key=97e4335bd73002b6731fc902f3748e96&language=en-US&query=The%20Shawshank%20Redemption&page=1&include_adult=false&year=1995'
 
 url = 'https://api.themoviedb.org/3/search/movie?'
 url += queryString
 
 r = requests.get(url)
 print r.status_code
-print r.content
+result = json.loads(r.content)
 
-##print url
+print result['results'][0]['title']
 
 
