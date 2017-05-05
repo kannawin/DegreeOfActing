a821cfa31f3b847ebb2186885fc21576acb108d8
kannawin <scott.kannawin@ou.edu>
2017-05-05 17:12:18 -0500

    Recursive actor call

--- getMov.py
+++ getMov.py
@@ -9,24 +9,31 @@ year = 1995
 search = 'The Shawshank Redemption'
 api = '97e4335bd73002b6731fc902f3748e96'
 
-pairs = {
-	'api_key' : api,
-	"year" : "1995",
-	"include_adult":"false",
-	"query" : search,
-	"language" : "en-US",
-}
 
-queryString = urllib.urlencode(pairs)
+for m in range(1,4000):
+	pairs = {
+		'api_key' : api,
+		"language" : "en-US",
+	}
 
-
-url = 'https://api.themoviedb.org/3/search/movie?'
-url += queryString
-
-r = requests.get(url)
-print r.status_code
-result = json.loads(r.content)
-
-print result['results'][0]['title']
+	queryString = urllib.urlencode(pairs)
 
 
+	url = 'https://api.themoviedb.org/3/person/'+str(m)+'/combined_credits?'
+	url += queryString
+	
+	r = requests.get(url)
+	if(r.status_code == 200):
+		result = json.loads(r.content)
+
+		films = []
+		media = []
+		job = []
+
+		for i in result['cast']:
+			films.append(i['id'])
+			media.append(i['media_type'])
+
+		print len(films)
+
+	
